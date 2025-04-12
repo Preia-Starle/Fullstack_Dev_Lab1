cd61020589b61105bce26b1fce0d6b9db4626b6e
Kate Arvay <kate@arvay.cz>
2025-04-12 18:21:06 +0200

    serve and render database data on frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./db');
 const routes = require('./routes');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -8,11 +9,19 @@ const app = express();
 //connect to DB
 connectDB();
 
+//serve frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 //middleware to parse JSON requests
 app.use(express.json());
 
 //placeholder for connecting routes
 app.use('/api', routes);
+
+//send index.html to the client
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/index.html'));  
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
